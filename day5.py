6baec64a00c25c998a4f4b3e9ec0d28b56b68bfa
Stylianos Sidiropoulos <stsid95@gmail.com>
2021-12-07 23:59:21 +0100

    day5

--- day5.py
+++ day5.py
@@ -18,38 +18,63 @@ with open("input/input5.txt", "r") as f:
         x2_data.append(line_x2)
         y2_data.append(line_y2)
 
-x1_new = []
-y1_new = []
-x2_new = []
-y2_new = []
-n = len(x1_data)
-for i in range(n):
-    if x1_data[i] == x2_data[i] or y1_data[i] == y2_data[i]:
-        x1_new.append(x1_data[i])
-        y1_new.append(y1_data[i])
-        x2_new.append(x2_data[i])
-        y2_new.append(y2_data[i])
 
-n = max(x1_new + y1_new + x2_new + y2_new)
-lines = [[0 for i in range(n+1)] for j in range(n+1)]
+def n_points_overlap(lines_points):
+    return sum([1 for item, count in Counter(tuple(item) for item in lines_points).items() if count >= 2])
+
 
 lines_points = []
-for x1, y1, x2, y2 in zip(x1_new, y1_new, x2_new, y2_new):
+for x1, y1, x2, y2 in zip(x1_data, y1_data, x2_data, y2_data):
     if x1 == x2:
         if y1 < y2:
-            for y in range(y1, y2+1):
+            for y in range(y1, y2 + 1):
                 lines_points.append([x1, y])
         else:
-            for y in range(y2, y1+1):
+            for y in range(y2, y1 + 1):
                 lines_points.append([x1, y])
     if y1 == y2:
         if x1 < x2:
-            for x in range(x1, x2+1):
+            for x in range(x1, x2 + 1):
                 lines_points.append([x, y1])
         else:
-            for x in range(x2, x1+1):
+            for x in range(x2, x1 + 1):
                 lines_points.append([x, y1])
 
-n_points_overlap = sum([1 for item, count in Counter(tuple(item) for item in lines_points).items() if count >= 2])
 print(f"Part 1: Consider only horizontal and vertical lines. "
-      f"At how many points do at least two lines overlap?: {n_points_overlap}")
+      f"At how many points do at least two lines overlap?: {n_points_overlap(lines_points)}")
+
+lines_points = []
+for x1, y1, x2, y2 in zip(x1_data, y1_data, x2_data, y2_data):
+    if x1 < x2:
+        if y1 < y2:
+            for x, y in zip(range(x1, x2 + 1), range(y1, y2 + 1)):
+                lines_points.append([x, y])
+        elif y1 > y2:
+            for x, y in zip(range(x1, x2 + 1), range(y1, y2 - 1, -1)):
+                lines_points.append([x, y])
+        else:
+            for x in range(x1, x2 + 1):
+                lines_points.append([x, y1])
+    elif x1 > x2:
+        if y1 < y2:
+            for x, y in zip(range(x1, x2 - 1, -1), range(y1, y2 + 1)):
+                lines_points.append([x, y])
+        elif y1 > y2:
+            for x, y in zip(range(x1, x2 - 1, -1), range(y1, y2 - 1, -1)):
+                lines_points.append([x, y])
+        else:
+            for x in range(x1, x2 - 1, -1):
+                lines_points.append([x, y1])
+    else:
+        if y1 < y2:
+            for y in range(y1, y2 + 1):
+                lines_points.append([x1, y])
+        elif y1 > y2:
+            for y in range(y1, y2 - 1, -1):
+                lines_points.append([x1, y])
+        else:
+            lines_points.append([x1, y1])
+
+
+print(f"Part 2: You still need to determine the number of points "
+      f"where at least two lines overlap: {n_points_overlap(lines_points)}")
