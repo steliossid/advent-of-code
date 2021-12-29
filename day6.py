59d59707857ae878b85ae3e3c2e5b54462c5861c
Stylianos Sidiropoulos <stsid95@gmail.com>
2021-12-29 10:22:26 +0200

    fix day 6

--- day6.py
+++ day6.py
@@ -1,4 +1,4 @@
-from _collections import Counter
+from collections import Counter
 
 with open("input/input6.txt", "r") as f:
     fish = []
@@ -6,21 +6,15 @@ with open("input/input6.txt", "r") as f:
         for number in line.split(","):
             fish.append(int(number))
 
-fish = Counter(fish)
+fc = Counter(fish)
+fc = [fc[i] for i in range(9)]
 
+def count_fish(fc, days):
+    for i in range(days):
+        fc = fc[1:] + [fc[0]]
+        fc[6] += fc[8]
+    return(sum(fc))
 
-def count_fish(fish, days):
-    for day in range(days):
-        n_current = len(fish)
-        for i in range(n_current):
-            if fish[i] == 0:
-                fish[i] = 6
-                fish.append(8)
-            else:
-                fish[i] -= 1
-    return len(fish)
+print(f"Part 1: How many lanternfish would there be after 80 days?: {count_fish(fc, days=80)}")
 
-
-print(f"Part 1: How many lanternfish would there be after 80 days?: {count_fish(fish, days=80)}")
-
-#print(f"Part 1: How many lanternfish would there be after 256 days?: {count_fish(fish, days=256)}")
+print(f"Part 1: How many lanternfish would there be after 256 days?: {count_fish(fc, days=256)}")
